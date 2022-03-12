489efc0a08daf0b0953948ab58697856ba25a352
loijilai <loijilai@gmail.com>
2022-03-12 20:47:45 +0800

    Edit hw1

--- problem1.R
+++ problem1.R
@@ -3,29 +3,51 @@ suidata <- c(5427, 5688, 6198, 6462, 6635, 7336, 7248, 7491, 8161, 8578, 9000)
 
 #2. & #3.
 set.seed(2015)
-Matrix <- runif(11)
-for(i in 1:99999){
-    Matrix <- rbind(Matrix, runif(11))
+
+######Better method for constructing the matrix######
+Matrix <- matrix(0, 100000, 11)
+for (i in 1:100000){
+    Matrix[i,] <- runif(11)
 }
 Matrix #Check the constructed matrix
 dim(Matrix) #100000 observations and 11 variables
 
+######################################################
+######Previous method for constructing the matrix######
+# Matrix <- runif(11)
+# for(i in 1:99999){
+#     Matrix <- rbind(Matrix, runif(11))
+# }
+######################################################
+
 #4.
-cor_vector <- cor(suidata, Matrix[i,])
-for(i in 2:100000){
-    cor_vector <- append(cor_vector, cor(suidata, Matrix[i,]))
+####Better method for constructing the vector#######
+cor_vector <- rep(0, 100000)
+for(i in 1:100000){
+    cor_vector[i] <- cor(suidata, Matrix[i,])
 }
+#####################################################
+
+####Previous method for constructing the vector#######
+# cor_vector <- cor(suidata, Matrix[1,])
+# for(i in 2:100000){
+#     cor_vector <- append(cor_vector, cor(suidata, Matrix[i,]))
+# }
+#####################################################
 
 #5. 
 #5.1
-length(cor_vector[cor_vector>0.9]) #Return 13
+length(cor_vector[cor_vector>0.9]) #Return 8
+
 
 #5.2
 which(cor_vector>0.9) 
 #Return index of elements where correlation > 0.9
+# [1] 14946 19593 31371 57252 72268 77349 90613
+# [8] 93402
 
 #5.3
-mean(cor_vector) 
+mean(cor_vector)  #-0.001601153
 
 #5.4
 Matrix[which.max(cor_vector),]
