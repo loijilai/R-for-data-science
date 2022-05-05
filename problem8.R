c6c9e1733740d54fc53ce474e4c8ae3a31be1718
Abner Teng <abner@dengyuchendeMacBook-Air.local>
2022-05-05 13:46:56 +0800

    problem8

--- problem8.R
+++ problem8.R
@@ -2,21 +2,22 @@ library(tidyverse)
 library(dplyr)
 library(ggthemes)
 data <- read_xls("/Users/abner/Downloads/np11.xls")
-C <- data$...4[66:77]
-C <- as.numeric(C)
+Consumption <- data$...4[66:77]
+Consumption <- as.numeric(C)
 fix_cap <- as.numeric(data$...6)
 inv <- as.numeric(data$...7)
-I <- fix_cap[66:77] + inv[66:77]
-G <- data$...5[66:77]
-G <- as.numeric(G)
+Investment <- fix_cap[66:77] + inv[66:77]
+Government_Spending <- data$...5[66:77]
+Government_Spending <- as.numeric(G)
 export <- as.numeric(data$...8)
 import <- as.numeric(data$...9)
-NX <- export[66:77] - import[66:77]
-growth_rate <- C+I+G+NX
+Net_Export <- export[66:77] - import[66:77]
+growth_rate <- Consumption+Investment+Government_Spending+Net_Export
 year <- c(2010:2021)
-tidy <- data.frame(year, C, I, G, NX, growth_rate)
-new_tidy <- gather(tidy, key = "exp", value = "pct",C:NX) %>% arrange(year)
+tidy <- data.frame(year, Consumption, Investment, Government_Spending, Net_Export, growth_rate)
+new_tidy <- gather(tidy, key = "exp", value = "pct",Consumption:Net_Export) %>% arrange(year)
 ggplot(data = new_tidy) +
   geom_bar(aes(x = year, y = pct, fill = exp),stat = "identity") +
+  labs(x = "", y = "", title = "Taiwan GDP : Contribution to Economic Growth, % points") +
   theme_economist() +
-  geom_line(mapping = aes(x = year, y = growth_rate), col = "blue")
+  geom_line(mapping = aes(x = year, y = growth_rate), col = "blue") 
